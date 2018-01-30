aa58b6ccb2fabf4c848e0fd50bb3e587b9818138
student <student@M11031>
2018-01-30 14:32:07 +0900

    b

--- src/tcpip1/Server.java
+++ src/tcpip1/Server.java
@@ -1,5 +1,61 @@
 package tcpip1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-  
+
+	public static void main(String[] args) {
+
+		ServerSocket serverSocket = null;
+		Socket socket = null;
+		InputStream in = null;
+		InputStreamReader inr = null;
+		BufferedReader br = null;
+
+		try {
+			serverSocket = new ServerSocket(7777);
+			InetAddress ia = InetAddress.getLocalHost();
+			System.out.println(ia.getHostName());
+			System.out.println(ia.getHostAddress());
+
+			System.out.println("Server Start!");
+
+			while (true) {
+				socket = serverSocket.accept();
+
+				System.out.println("Client Connected");
+
+				// Receive Data
+				in = socket.getInputStream();
+				inr = new InputStreamReader(in);
+				br = new BufferedReader(inr);
+				String receiveStr = br.readLine();
+				System.out.println(receiveStr);
+
+				System.out.println("End Server");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (br != null)
+					br.close();
+				if (inr != null)
+					inr.close();
+				if (in != null)
+					in.close();
+				if (socket != null)
+					socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
 }
